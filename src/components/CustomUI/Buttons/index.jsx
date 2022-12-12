7837dbe12d89c4d8e2e629ad3eb17f3dfbe5b5c1
Davyd Bredykhin <correodemimobil@gmail.com>
2022-12-12 16:14:41 +0100

    customButton styles done. final version

--- src/components/CustomUI/Buttons/index.jsx
+++ src/components/CustomUI/Buttons/index.jsx
@@ -1,23 +1,34 @@
 import styles from './Buttons.module.css';
 import React from 'react'
 
-export const LinkButton = ({text, link}) => {
+export const CustomButton = ({type, text, link, style}) => {
+  if (style === "btn") {
+    return (
+      <button className={`${styles.button} ${styles.buttonBtn}`} type={type}>
+        <span>{text}</span>
+        <svg width="13px" height="10px" viewBox="0 0 13 10">
+          <path d="M1,5 L11,5"></path>
+          <polyline points="8 1 12 5 8 9"></polyline>
+        </svg>
+      </button>
+    );
+  }
+  else if (style === "link") {
+    return (
+      <a href={link} className={`${styles.button} ${styles.buttonLink}`} target="_blank" rel="noreferrer">
+        <span>{text}</span>
+        <svg width="13px" height="10px" viewBox="0 0 13 10">
+          <path d="M1,5 L11,5"></path>
+          <polyline points="8 1 12 5 8 9"></polyline>
+        </svg>
+      </a>
+    )
+  }
   return (
-    <a className={styles.linkButton} href={link} target="_blank" rel="noreferrer">
-      <span>{text}</span>
-      <i className="fa-solid fa-arrow-right-long"></i>
-    </a>
-  );
-}
-
-export const CircleButton = ({text, type}) => {
-  return (
-    <button className={styles.circleButton} type={type}>
-      <span className={styles.circle} aria-hidden="true">
-        <span className={`${styles.arrow} ${styles.icon}`}></span>
-      </span>
-      <span className={styles.buttonText}>{text}</span>
+    <button>
+      {text}
     </button>
-  );
+  )
+
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { LinkButton, CircleButton } from './CustomUI/Buttons';
+export { CustomButton } from './CustomUI/Buttons';
 export { Checkbox } from './CustomUI/Checkbox';
 export { SectionTitle } from './SectionTitle';
 export { ContactForm } from './ContactForm';
