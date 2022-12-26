8d9415812d28fcdd6da0cda51ddc6b8190c125b7
Davyd Bredykhin <correodemimobil@gmail.com>
2022-12-26 17:13:20 +0100

    error page fixed

--- src/index.js
+++ src/index.js
@@ -2,29 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Main, ServicesAdditionalPage, TrainingAdditionalPage, ModalPage, NotFoundPage } from './routes'
-import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Main/>,
-  },
-  {
-    path: '/services',
-    element: <ServicesAdditionalPage/>
-  },
-  {
-    path: '/training',
-    element: <TrainingAdditionalPage/>
-  },
-  {
-    path: '/modal',
-    element: <ModalPage/>,
-  },
-  {
-    path: '/*',
-    element: <NotFoundPage/>,
-  },
+    errorElement: <NotFoundPage/>,
+    children: [
+      {
+        index: true,
+        element: <Main/>,
+      },
+      {
+        path: '/services',
+        element: <ServicesAdditionalPage/>
+      },
+      {
+        path: '/training',
+        element: <TrainingAdditionalPage/>
+      },
+      {
+        path: '/modal',
+        element: <ModalPage/>,
+      },
+    ]
+  }
 
 ]);
 
