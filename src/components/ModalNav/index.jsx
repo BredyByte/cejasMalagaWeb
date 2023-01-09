665c254911ae13b538720aa0a3e7ca5a76c0107a
Davyd Bredykhin <correodemimobil@gmail.com>
2023-01-09 19:08:07 +0100

    map links changed

--- src/components/ModalNav/index.jsx
+++ src/components/ModalNav/index.jsx
@@ -28,7 +28,7 @@ export const ModalNav = ({reference}) => {
           <ul className={styles.connectionList}>
             <li className={styles.connectionItem}>
               <ListStar/>
-              <a href='https://www.google.com/maps/place/%D0%9A%D1%80%D0%B8%D1%81%D1%82%D0%BE+%D0%B4%D0%B5+%D0%BB%D0%B0+%D0%AD%D0%BF%D0%B8%D0%B4%D0%B5%D0%BC%D0%B8%D1%8F,+%D0%9C%D0%B0%D0%BB%D0%B0%D0%B3%D0%B0,+%D0%98%D1%81%D0%BF%D0%B0%D0%BD%D0%B8%D1%8F/@36.7292463,-4.4165367,17z/data=!3m1!4b1!4m5!3m4!1s0xd72f7c692acd80b:0x760121b3b7423ef0!8m2!3d36.7305945!4d-4.4149938' target="_blank" rel="noreferrer">
+              <a href='https://goo.gl/maps/e6GZSVoznfLFvHn27' target="_blank" rel="noreferrer">
                 calle cristo de la epidemia 93
               </a>
             </li>
